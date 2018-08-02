3cf64a718251c42f32a21152f97879c7b9c0f244
nicklennon <nicklennon.53@gmail.com>
2018-08-02 12:16:23 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,16 @@ class ApplicationController < Sinatra::Base
   	set :public_dir, "public"
   end
 
+ get '/imagelink' do
+    erb :deepfry
+  end
+
+  # Add your post route and action be
+  post '/imagefinal' do
+    @link = params[:link]
+    erb :imagefinal
+    # @fav_food = params[:fav_food]
+    # "My name is #{@name} and I love #{@fav_food}."
+  end
   
 end
